cbb26a2d0e232276c55103323e5708286a2aa087
Ryan Yeh <ej68433221ik@gmail.com>
2022-09-17 17:19:58 +0800

    完成靜態頁面設置

--- application.js
+++ application.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const path = require('path');
-const pagesRouter = require('./apis/pages');
-const apisRouter = require('./apis/api');
+const pageRouter = require('./apis/pages');
+const apiRouter = require('./apis/api');
 const PORT = 3000;
 const app = express();
 
 // 掛載 ejs
 app.set("view engine", "ejs");
+
 // 使用 boostrap
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
 // 使用靜態資源
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.urlencoded({extended: true})); // 取代 body-parser
+app.use(express.json());
+
+// 路由設定
+app.use("/", pageRouter); // 前端頁面設定
+app.use("/api", apiRouter);// api設定
 
 app.listen(3000, () => {
   console.log(`Server is start on port ${PORT}`);
--- connection/mySqlConnetion.js
+++ connection/mySqlConnetion.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+const config = require('config');
 
 const connection = mysql.createConnection(
   {
-    host:config.get("host"), 
-    user:config.get("user"), 
-    password:config.get("password"),
-    database:config.get("database")
+    host: config.get("host"), 
+    user: config.get("user"), 
+    password: config.get("password"),
+    database: config.get("database")
   }
 );
 
